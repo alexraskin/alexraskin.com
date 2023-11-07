93d7152382a80f91b72eb93b29e18e4a8d9991d0
alexraskin <alexraskin@fastmail.fm>
2023-11-06 22:23:23 -0700

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,34 @@
+const about: string = `I have been tinkering with code for the past 8 years – and yup, I'm that person who gets giddy over Terraform, TypeScript, and Python. Backend's where the party's at, if you ask me.
+When I'm not in my coding groove, you'll catch me gaming with friends, or trying to impress my cat, Cosmo, with my subpar laser pointer skills.`
+
+export const info = {
+  baseUrl: "https://alexraskin.com",
+  logo: "GP.",
+  h1: "Hello, I'm Alex Raskin",
+  h2: "I'm a creative technologist",
+  h3: about,
+  ctas: [
+    {
+      label: "Phone",
+      href: "tel:+918888259999",
+    },
+    {
+      label: "Email",
+      href: "mailto:hi@gavn.in",
+    },
+  ],
+  socials: [
+    {
+      icon: "fab fa-youtube",
+      href: "https://youtube.com",
+    },
+    {
+      icon: "fab fa-twitter",
+      href: "https://twitter.com",
+    },
+    {
+      icon: "fab fa-facebook-f",
+      href: "https://facebook.com",
+    },
+  ],
+};
