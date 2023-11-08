59cfb666b01eb4dcbca341564ec458d0f0a3bf96
alexraskin <alexraskin@fastmail.fm>
2023-11-07 18:22:18 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexraskin.com",
-  integrations: [tailwind(), sitemap(), image()]
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: cloudflare()
 });
